1babe8a9d3090835762f41ffc2e7321106420ffe
kimwager <kim.wager@pharmagenesis.com>
2025-01-10 15:53:41 +0000

    Continue quiz responses

--- movie-lens-dataset-quiz.R
+++ movie-lens-dataset-quiz.R
@@ -1,3 +1,5 @@
+
+
 # Find number of rows and columns
 str(edx) 
 
@@ -11,3 +13,34 @@ sum(edx$rating == 3)
 edx %>% 
   distinct(movieId) %>% 
   nrow()
+
+# How many users?
+
+edx %>% 
+  distinct(userId) %>% 
+  nrow()
+
+# How many movie ratings are in each of the following genres in the edx dataset? Drama: Comedy: Thriller: Romance:
+
+edx %>%
+  summarize(
+    Drama = sum(str_detect(genres, "Drama")),
+    Comedy = sum(str_detect(genres, "Comedy")),
+    Thriller = sum(str_detect(genres, "Thriller")),
+    Romance = sum(str_detect(genres, "Romance"))
+  )
+
+# Which movie has the greatest number of ratings?
+
+  edx %>%
+    group_by(title) %>%
+    summarise(number_of_ratings = n()) %>%
+    arrange(desc(number_of_ratings)) %>%
+    slice(1)
+
+# What are the five most given ratings in order from most to least?
+edx %>%
+  group_by(rating) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  head(5)
